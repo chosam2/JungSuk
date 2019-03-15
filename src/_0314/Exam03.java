3f575aa1b94c1b3a7ae8df04c4a0760af165e6d7
hyungwook0221 <20140931@gm.hannam.ac.kr>
2019-03-15 15:33:12 +0900

    # 0315 정석

--- src/_0314/Exam03.java
+++ src/_0314/Exam03.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Exam03 {
 	public static void main(String[] args) {
 
-		int answer = (int) (Math.random() * 100) + 1;
-		int input = 0; // 사용자입력을 저장할 공간
-		int count = 0; // 시도횟수를 세기위한 변수
+		int rand = (int) (Math.random() * 100) + 1;
+		int input = 0;
+		int cnt = 0;
 		Scanner sc = new Scanner(System.in);
 		do {
-			count++;
+			cnt++;
 			System.out.print("1과 100사이의 값을 입력하세요 :");
 			input = Integer.parseInt(sc.nextLine()); // 입력받은 값을 변수 input에 저장한다.
-			if (answer > input) {
+			if (rand > input) {
 				System.out.println("더 큰 수를 입력하세요.");
-			} else if (answer < input) {
+			} else if (rand < input) {
 				System.out.println("더  작은 수를 입력하세요.");
 			} else {
 				System.out.println("맞췄습니다.");
-				System.out.println("시도횟수는 " + count + "번입니다.");
+				System.out.println("시도횟수는 " + cnt + "번입니다.");
 				break; // do-while문을 벗어난다
 			}
 		} while (true); // 무한반복문
