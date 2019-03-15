dce6ee5ed2e60d42d3c283ca231cec29f685fa5f
hyungwook0221 <20140931@gm.hannam.ac.kr>
2019-03-15 17:19:39 +0900

    # 0315 정석

--- src/ch06/Exercise6_19.java
+++ src/ch06/Exercise6_19.java
@@ -3,25 +3,19 @@ package ch06;
 class Exercise6_19 {
 	public static void change(String str) {
 		str += "456";
-		System.out.println(str.toString());
+		System.out.println(str.hashCode());
 
 	}
 
 	public static void main(String[] args) {
 		Excrcise6_6 ex = new Excrcise6_6();
-		System.out.println(ex.toString());
 
 		String str = "ABC123";
-		System.out.println(str);
+		System.out.println(str.hashCode());
 
 		change(str);
 		System.out.println("After change : " + str);
-		System.out.println(str.toString() + "!!!!!!!!!!");
-
-	}
-
-	public String toString() {
-		return getClass().getName() + "@" + Integer.toHexString(hashCode());
+		System.out.println(str.hashCode());
 	}
 
 }
