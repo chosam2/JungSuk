285a622db16e49082751d39241f1fb9fd055a4db
hyungwook0221 <20140931@gm.hannam.ac.kr>
2019-04-05 17:22:56 +0900

    # 0405 자바의 정석

--- src/ch07/Exercise7_16.java
+++ src/ch07/Exercise7_16.java
@@ -10,9 +10,14 @@ package ch07;
 	b. fe instanceof Movable
 	c. fe instanceof Object
 	d. fe instanceof Car
-	e. fe instanceof Ambulance
+	e. fe instanceof Ambulance	=> Ambulance는 FireEngine과 같은 레벨
  *
  */
 public class Exercise7_16 {
 
 }
+
+//class Car{}
+//class FireEngine extends Car implements Movable{}
+//class Ambulance extends Car{}
+//FireEngine fe = new FireEngine();
